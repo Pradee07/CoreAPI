77934f7dc6a521bff60d6e8250a48e4f1a89a492
Pradeep <pradee.ns@gmail.com>
2018-03-05 19:45:26 +0530

    HTTP verb attributes for handling same parameter GET api endpoints

---
+++ CoreAPI/CoreAPI/Controllers/ValuesController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "from first method";
+        }
+
+        // GET api/values/second/5
+        [HttpGet("second/{id:int}")]
+        public string GetSecond(int id)
+        {
+            return "from second method";
+        }
+
+        // GET api/values/third/5
+        [HttpGet("third/{id:int}")]
+        public string GetThird(int id)
+        {
+            return "from third method";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
