7a2bbafd14d3e2e648dcb61850c8ec52ec7d552e
Pradeep <pradee.ns@gmail.com>
2018-03-06 17:19:51 +0530

    Core API - Passing Complex objects, Injecting dependency

---
+++ CoreAPI/CoreAPI/Models/Contracts.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreAPI.Models
+{
+    public interface IEmployee
+    {
+        void DoImpementaion(); //Mock method
+    }
+    public class Employee : IEmployee
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+
+        public void DoImpementaion()
+        {
+            //Implementation 
+        }
+    }
+
+    public class EmployeeDto
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }        
+    }
+}
--- CoreAPI/CoreAPI/Startup.cs
+++ CoreAPI/CoreAPI/Startup.cs
@@ -2,12 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CoreAPI.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+
 
 namespace CoreAPI
 {
@@ -24,6 +28,7 @@ namespace CoreAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddTransient<IEmployee, Employee>();          
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
